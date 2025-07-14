cdcdc25abf8f15f3dacfcda460d94bf7a672fa95
biffin2022cse <130990891+biffin2022cse@users.noreply.github.com>
2025-07-14 22:06:56 +0530

    Update sc.js

--- sc.js
+++ sc.js
@@ -2,19 +2,22 @@ let timerEl = document.getElementById("timer");
 let defuserEl = document.getElementById("defuser");
 let countdown = 10;
 
-let intervalId = setInterval(function() {
-  countdown = countdown - 1;
+let intervalId = setInterval(function () {
+  countdown--;
   timerEl.textContent = countdown;
   if (countdown === 0) {
-    timerEl.textContent = "BOOM";
+    timerEl.textContent = "💥 BOOM!";
     clearInterval(intervalId);
   }
 }, 1000);
 
-defuserEl.addEventListener("keydown", function(event) {
-  let bombDefuserText = defuserEl.value;
-  if (event.key === "Enter" && bombDefuserText === "defuse" && countdown !== 0) {
-    timerEl.textContent = "You did it!";
+defuserEl.addEventListener("keydown", function (event) {
+  if (
+    event.key === "Enter" &&
+    defuserEl.value.trim().toLowerCase() === "defuse" &&
+    countdown > 0
+  ) {
+    timerEl.textContent = "✅ You did it!";
     clearInterval(intervalId);
   }
 });
